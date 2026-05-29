e3b4eeb98c860c6ce46016ea983976f6f794eb40
Girish Parate <girishparate@Girishs-MacBook-Pro.local>
2026-05-29 09:28:27 +0530

    Native ios code added

---
+++ ios/NativeBuildEnv/NativeBuildInfoModule.mm
@@ -0,0 +1,23 @@
+//
+//  RCTNativeBuildEnv.m
+//  ReactNativeStarter
+//
+//  Created by Girish Parate on 28/05/26.
+//
+
+#import "NativeBuildInfoModule.h"
+
+@implementation NativeBuildInfoModule
+
+RCT_EXPORT_MODULE(BUILD_ENV)
+
+- (NSString *)getBuildType {
+    return [[NSBundle mainBundle] objectForInfoDictionaryKey:@"BUILD_ENV"];
+}
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+    (const facebook::react::ObjCTurboModule::InitParams &)params {
+    return std::make_shared<facebook::react::NativeBuildEnvSpecJSI>(params);
+}
+
+@end
